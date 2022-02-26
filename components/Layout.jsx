0664244a784e000affd36b3595fe0b7df6c7c393
Doug Leinen <doug@geletkaplus.com>
2022-02-26 16:43:40 -0600

    add footer markup

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,7 +4,7 @@ import Nav from "./Nav";
 const Layout = ({ children }) => {
   return (
     <>
-      <h1>Green Shirt Studios</h1>
+      <h1>Green Shirt Studio</h1>
       <Nav />
       {children}
       <footer>
@@ -31,6 +31,12 @@ const Layout = ({ children }) => {
           <h3>Visit Us</h3>
           <small>4001 N Raventswood Ave, Unit 303-B Chicago, IL 60613</small>
           {/* map goes here */}
+        </div>
+        <div className="contact">
+          <h3>Contact Us</h3>
+          <small>info@greenshirtstudio.com</small>
+          <small>773-217-9565</small>
+          <h3>Follow Us</h3>
         </div>
       </footer>
     </>
