cddd1d2b3222cdd656682e4f08eb40d96a0e6d3d
Doug Leinen <doug@geletkaplus.com>
2022-08-07 18:06:39 -0500

    style class header

--- src/components/classComponents/ClassHeader.jsx
+++ src/components/classComponents/ClassHeader.jsx
@@ -9,20 +9,35 @@ const StyledClassHeader = styled.div`
   align-items: center;
 
   h2 {
-    font-size: 3.75rem;
-    line-height: 4.875rem;
+    font-size: 2rem;
+    line-height: 2rem;
+    margin-bottom: 1rem;
   }
 
   img {
     width: 50%;
+    padding-right: 1rem;
   }
 
   .info {
+    padding-left: 1rem;
     padding-right: 2.25rem;
     width: 50%;
 
     p {
       line-height: 1.875rem;
+      font-size: 1.25rem;
+    }
+
+    .price {
+      margin-top: 2rem;
+      font-size: 3rem;
+      line-height: 2rem;
+
+      small {
+        font-size: 1rem;
+        color: var(--dark-gray);
+      }
     }
   }
 `;
@@ -36,6 +51,13 @@ const ClassHeader = ({ wpClass }) => {
         <p>{`${wpClass.classGroup.day}, ${wpClass.classGroup.dates[0].date} - ${
           wpClass.classGroup.dates[wpClass.classGroup.dates.length - 1].date
         }, ${wpClass.classGroup.time} with ${wpClass.author.node.name}`}</p>
+
+        <div className="price">
+          ${wpClass.classGroup.price} <br />
+          <small>or $110 every 3 weeks</small>
+        </div>
+
+        <button className={"button fill"}>Sign Up</button>
       </div>
     </StyledClassHeader>
   );
