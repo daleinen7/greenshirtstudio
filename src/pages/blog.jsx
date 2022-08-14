93943295cb158fa362f302a3f8c5fecbea106c9a
Doug Leinen <doug@geletkaplus.com>
2022-08-13 23:50:52 -0500

    responsive blog page

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -3,45 +3,23 @@ import { Link } from "gatsby";
 import Layout from "../components/Layout";
 import ContentStack from "../components/ContentStack";
 import { graphql } from "gatsby";
-
-import styled from "styled-components";
 import BlogCard from "../components/BlogCard";
+import HeadingEffect from "../components/HeadingEffect";
+import styled from "styled-components";
 
-const BlogPreview = styled.article`
-  h3 {
-    font-size: 1.25rem;
-    line-height: 1.857rem;
-    font-weight: 400;
-    text-align: left;
-  }
-  a {
-    color: var(--black);
-    text-decoration: none;
-    :hover {
-      text-decoration: underline;
-    }
-  }
+const StyledHero = styled.div`
+  display: flex;
+  justify-content: center;
+  padding-top: 5rem;
+  padding-bottom: 3rem;
 `;
 
 const Blog = ({ data }) => {
-  console.log(data);
-  const blogPosts = data.allWpPost.nodes.map((post) => (
-    <BlogPreview>
-      <Link to={`${post.slug}`}>
-        {post.featuredImage?.node.sourceUrl && (
-          <img src={post.featuredImage?.node.sourceUrl} alt={post.title} />
-        )}
-      </Link>
-      <Link to={`${post.slug}`}>
-        <h3>{post.title}</h3>
-      </Link>
-      <small>{post.author.node.name}</small>
-    </BlogPreview>
-  ));
-
   return (
     <Layout>
-      <h2>Blog</h2>
+      <StyledHero>
+        <HeadingEffect text={"Blog"} />
+      </StyledHero>
       <ContentStack
         content={data.allWpPost.nodes.map((post) => {
           return (
