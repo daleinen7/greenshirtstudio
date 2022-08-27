a401ae52165aee084036380febf6574c15356fec
Doug Leinen <doug@geletkaplus.com>
2022-08-27 16:24:29 -0500

    start wp api

--- netlify/functions/handle-purchase.js
+++ netlify/functions/handle-purchase.js
@@ -1,6 +1,10 @@
+// import apiFetch from "@wordpress/api-fetch";
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async ({ body, headers }) => {
+  console.log("BODY: ", body);
+  // console.log("HEADERS: ", headers);
+
   try {
     // check the webhook to make sure it’s valid
     const stripeEvent = stripe.webhooks.constructEvent(
@@ -9,12 +13,14 @@ exports.handler = async ({ body, headers }) => {
       process.env.STRIPE_WEBHOOK_SECRET
     );
 
-    const date = new Date();
-    const oneMonthOut = new Date(date.setMonth(date.getMonth() + 1));
-
     // only do stuff if this is a successful Stripe Checkout purchase
     if (stripeEvent.type === "checkout.session.completed") {
       const eventObject = stripeEvent.data.object;
+
+      if (body.data.object.data.subscription) {
+        const date = new Date();
+        const oneMonthOut = new Date(date.setMonth(date.getMonth() + 1));
+      }
 
       const subscription = await stripe.subscriptions.retrieve(
         eventObject.subscription
@@ -24,6 +30,19 @@ exports.handler = async ({ body, headers }) => {
         cancel_at: oneMonthOut,
       });
     }
+
+    // // POST
+    // apiFetch({
+    //   path: "/wp/v2/book/{ID}",
+    //   method: "GET",
+    //   data: {
+    //     acf: {
+    //       author: "Abraham (Abe) Simpson",
+    //     },
+    //   },
+    // }).then((res) => {
+    //   console.log(res);
+    // });
 
     return {
       statusCode: 200,
