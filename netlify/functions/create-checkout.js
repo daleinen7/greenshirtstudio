9ec48ea915de440c03191278065c39e84a72f984
Doug Leinen <doug@geletkaplus.com>
2022-09-17 08:13:03 -0500

    add try catch

--- netlify/functions/create-checkout.js
+++ netlify/functions/create-checkout.js
@@ -8,23 +8,28 @@ exports.handler = async ({ body, headers }) => {
 
   console.log("Params: ", params);
 
-  const session = await stripe.checkout.sessions.create({
-    success_url: `${process.env.GATSBY_URL_ENVIRONMENT}/success?session_id={CHECKOUT_SESSION_ID}`,
-    cancel_url: `${process.env.GATSBY_URL_ENVIRONMENT}/cancel`,
-    payment_method_types: ["card"],
-    line_items: params.lineItems,
-    mode: params.paymentType,
-    allow_promotion_codes: params.promotion,
-    metadata: { dayOfWeek: params.dayOfWeek, databaseId: params.dbid },
-  });
+  try {
+    const session = await stripe.checkout.sessions.create({
+      success_url: `${process.env.GATSBY_URL_ENVIRONMENT}/success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${process.env.GATSBY_URL_ENVIRONMENT}/cancel`,
+      payment_method_types: ["card"],
+      line_items: params.lineItems,
+      mode: params.paymentType,
+      allow_promotion_codes: params.promotion,
+      metadata: { dayOfWeek: params.dayOfWeek, databaseId: params.dbid },
+    });
 
-  console.log("SESSION: ", session);
+    console.log("SESSION: ", session);
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      sessionId: session.id,
-      publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-    }),
-  };
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        sessionId: session.id,
+        publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+      }),
+    };
+  } catch (error) {
+    console.log("This failed I see", err);
+    res.status(500).json({ message: err.message });
+  }
 };
