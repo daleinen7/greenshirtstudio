e62c02ff709963cd3c49ac65bf7cfa71d23293b2
kndshein <kndshein@gmail.com>
2025-01-14 22:04:14 -0800

    Fix isValidDate further

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -6,9 +6,19 @@ export function producePositionString(positions) {
   return positions.map((position) => position.name).join(', ');
 }
 
-function isValidDate(date) {
-  const parsedDate = new Date(date);
-  return parsedDate instanceof Date && !isNaN(parsedDate);
+function isValidDate(date_string) {
+  // Regular expression to match the format YYYY-MM-DD
+  const regex = /^\d{4}-\d{2}-\d{2}$/;
+  if (!regex.test(date_string)) return false;
+
+  const [year, month, day] = date_string.split('-').map(Number);
+  // Check for valid month (01 to 12)
+  if (month < 1 || month > 12) return false;
+
+  const days_in_month = new Date(year, month, 0).getDate(); // Get the number of days in the given month
+  if (day < 1 || day > days_in_month) return false;
+
+  return true;
 }
 
 export function dashToSlashDate(date) {
